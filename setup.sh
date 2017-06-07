253b13fee223f2b3e578d8a660c66254c1905239
Nico Alt <nicoalt@posteo.org>
2017-06-07 20:27:48 +0200

    Upgrade python dependencies in setup.sh
    Reference: https://gitlab.com/fdroid/repomaker/merge_requests/91#note_31622916

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-pip3 install -r requirements.txt --user && \
+pip3 install -r requirements.txt --user --upgrade --upgrade-strategy only-if-needed && \
 python3 manage.py makemigrations maker && \
 python3 manage.py migrate && \
 echo "All set up, now execute run.sh"
