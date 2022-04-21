28db9d259c5df568644e2541a7c169feaee4093e
Steven McDonald <steven.mcdonald@gmail.com>
2022-04-21 14:39:55 -0700

    django plugin has to be explicitly loaded now

--- tests/test-pylint.sh
+++ tests/test-pylint.sh
@@ -4,4 +4,4 @@ pylint=pylint
 if which pylint3; then
     pylint=pylint3
 fi
-$pylint --disable=C,R,fixme repomaker
+$pylint --load-plugins=pylint_django --django-settings-module=repomaker.settings_test --disable=C,R,fixme repomaker
