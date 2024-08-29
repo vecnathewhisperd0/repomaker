9afd276fcf1951644f916e95ecb3e4161e2e9e6f
thefuture <pmmayero@gmail.com>
2024-08-30 01:30:48 +0300

    fix pep8 issues

--- repomaker/urls.py
+++ repomaker/urls.py
@@ -29,63 +29,80 @@ urlpatterns = [
     re_path(r'^tinymce/', include('tinymce.urls')),
     re_path(r'^jsreverse/$', urls_js, name='js_reverse'),
     re_path(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], media_serve,
-        {'document_root': settings.MEDIA_ROOT}, name='media'),
+            {'document_root': settings.MEDIA_ROOT}, name='media'),
 
     # JavaScript Internationalisation
-    re_path(r'^jsi18n/$', JavaScriptCatalog.as_view(packages=['repomaker']), name='javascript-catalog'),
+    re_path(
+        r'^jsi18n/$',
+        JavaScriptCatalog.as_view(
+            packages=['repomaker']),
+        name='javascript-catalog'),
 
     # Repo
     re_path(r'^$', RepositoryListView.as_view(), name='index'),
     re_path(r'^add$', RepositoryCreateView.as_view(), name='add_repo'),
     re_path(r'^(?P<repo_id>[0-9]+)/$', RepositoryView.as_view(), name='repo'),
     re_path(r'^(?P<repo_id>[0-9]+)/edit/$', RepositoryUpdateView.as_view(),
-        name='edit_repo'),
+            name='edit_repo'),
     re_path(r'^(?P<repo_id>[0-9]+)/delete/$', RepositoryDeleteView.as_view(),
-        name='delete_repo'),
+            name='delete_repo'),
 
     # Remote Repo
-    re_path(r'^remote/add$', RemoteRepositoryCreateView.as_view(), name='add_remote_repo'),
-    re_path(r'^remote/(?P<remote_repo_id>[0-9]+)/update/$', views.remote_update, name='remote_update'),
+    re_path(
+        r'^remote/add$',
+        RemoteRepositoryCreateView.as_view(),
+        name='add_remote_repo'),
+    re_path(
+        r'^remote/(?P<remote_repo_id>[0-9]+)/update/$',
+        views.remote_update,
+        name='remote_update'),
 
     # App
-    re_path(r'^(?P<repo_id>[0-9]+)/app/add/$', AppRemoteAddView.as_view(), name='add_app'),
+    re_path(
+        r'^(?P<repo_id>[0-9]+)/app/add/$',
+        AppRemoteAddView.as_view(),
+        name='add_app'),
     re_path(r'^(?P<repo_id>[0-9]+)/remote-app/(?P<remote_repo_id>[0-9]+)/add/$',
-        AppRemoteAddView.as_view(), name='add_app'),
+            AppRemoteAddView.as_view(), name='add_app'),
     re_path(r'^(?P<repo_id>[0-9]+)/remote-app/add/category/(?P<category_id>[0-9]+)/$',
-        AppRemoteAddView.as_view(), name='add_app_with_category'),
+            AppRemoteAddView.as_view(), name='add_app_with_category'),
     re_path(r'^(?P<repo_id>[0-9]+)/remote-app/(?P<remote_repo_id>[0-9]+)/add/' +
-        r'category/(?P<category_id>[0-9]+)/$',
-        AppRemoteAddView.as_view(), name='add_app_with_category'),
+            r'category/(?P<category_id>[0-9]+)/$',
+            AppRemoteAddView.as_view(), name='add_app_with_category'),
     # Remote App Details
     re_path(r'^(?P<repo_id>[0-9]+)/remote-app/(?P<remote_repo_id>[0-9]+)/(?P<app_id>[0-9]+)' +
-        r'/lang/(?P<lang>[a-zA-Z_-]+)/$',
-        RemoteAppImportView.as_view(), name='add_remote_app'),
+            r'/lang/(?P<lang>[a-zA-Z_-]+)/$',
+            RemoteAppImportView.as_view(), name='add_remote_app'),
     re_path(r'^(?P<repo_id>[0-9]+)/remote-app/(?P<remote_repo_id>[0-9]+)/(?P<app_id>[0-9]+)' +
-        r'/lang/(?P<lang>[a-zA-Z_-]+)/screenshots/$',
-        RemoteAppImportViewScreenshots.as_view(), name='add_remote_app_screenshots'),
+            r'/lang/(?P<lang>[a-zA-Z_-]+)/screenshots/$',
+            RemoteAppImportViewScreenshots.as_view(), name='add_remote_app_screenshots'),
     # App Detail and Edit
-    re_path(r'^(?P<repo_id>[0-9]+)/app/(?P<app_id>[0-9]+)/$', AppDetailView.as_view(), name='app'),
+    re_path(
+        r'^(?P<repo_id>[0-9]+)/app/(?P<app_id>[0-9]+)/$',
+        AppDetailView.as_view(),
+        name='app'),
     re_path(r'^(?P<repo_id>[0-9]+)/app/(?P<app_id>[0-9]+)/lang/(?P<lang>[a-zA-Z_-]+)/$',
-        AppDetailView.as_view(), name='app'),
+            AppDetailView.as_view(), name='app'),
     re_path(r'^(?P<repo_id>[0-9]+)/app/(?P<app_id>[0-9]+)/lang/(?P<lang>[a-zA-Z_-]+)/edit/$',
-        AppEditView.as_view(), name='app_edit'),
+            AppEditView.as_view(), name='app_edit'),
     re_path(r'^(?P<repo_id>[0-9]+)/app/(?P<app_id>[0-9]+)/add/lang/$',
-        AppTranslationCreateView.as_view(), name='app_add_lang'),
+            AppTranslationCreateView.as_view(), name='app_add_lang'),
     # App Delete
     re_path(r'^(?P<repo_id>[0-9]+)/app/(?P<app_id>[0-9]+)/delete/$', AppDeleteView.as_view(),
-        name='delete_app'),
+            name='delete_app'),
     # Feature Graphic Delete
     re_path(r'^(?P<repo_id>[0-9]+)/app/(?P<app_id>[0-9]+)/feature_graphic/delete/$',
-        AppFeatureGraphicDeleteView.as_view(), name='delete_feature_graphic'),
+            AppFeatureGraphicDeleteView.as_view(), name='delete_feature_graphic'),
 
     # App Screenshots
     re_path(r'^(?P<repo_id>[0-9]+)/app/(?P<app_id>[0-9]+)/screenshot/(?P<s_id>[0-9]+)/delete/$',
-        ScreenshotDeleteView.as_view(), name='screenshot_delete'),
+            ScreenshotDeleteView.as_view(), name='screenshot_delete'),
 
     # Apks
-    re_path(r'^(?P<repo_id>[0-9]+)/app/apk/upload/$', ApkUploadView.as_view(), name='apk_upload'),
+    re_path(r'^(?P<repo_id>[0-9]+)/app/apk/upload/$',
+            ApkUploadView.as_view(), name='apk_upload'),
     re_path(r'^(?P<repo_id>[0-9]+)/app/(?P<app_id>[0-9]+)/apk/(?P<pk>[0-9]+)/delete/$',
-        ApkPointerDeleteView.as_view(), name='apk_delete'),
+            ApkPointerDeleteView.as_view(), name='apk_delete'),
 
     # Repo Operations
     re_path(r'^(?P<repo_id>[0-9]+)/update/$$', views.update, name='update'),
@@ -93,37 +110,37 @@ urlpatterns = [
 
     # Storages
     re_path(r'^(?P<repo_id>[0-9]+)/storage/add/$',
-        StorageAddView.as_view(), name='storage_add'),
+            StorageAddView.as_view(), name='storage_add'),
 
     # S3 Storage
     re_path(r'^(?P<repo_id>[0-9]+)/storage/s3/add/$',
-        S3StorageCreate.as_view(), name=S3Storage.add_url_name),
+            S3StorageCreate.as_view(), name=S3Storage.add_url_name),
     re_path(r'^(?P<repo_id>[0-9]+)/storage/s3/(?P<pk>[0-9]+)/$',
-        S3StorageDetail.as_view(), name=S3Storage.detail_url_name),
+            S3StorageDetail.as_view(), name=S3Storage.detail_url_name),
     re_path(r'^(?P<repo_id>[0-9]+)/storage/s3/(?P<pk>[0-9]+)/edit/$',
-        S3StorageUpdate.as_view(), name=S3Storage.edit_url_name),
+            S3StorageUpdate.as_view(), name=S3Storage.edit_url_name),
     re_path(r'^(?P<repo_id>[0-9]+)/storage/s3/(?P<pk>[0-9]+)/delete/$',
-        S3StorageDelete.as_view(), name=S3Storage.delete_url_name),
+            S3StorageDelete.as_view(), name=S3Storage.delete_url_name),
 
     # SSH Storage
     re_path(r'^(?P<repo_id>[0-9]+)/storage/ssh/add/$',
-        SshStorageCreate.as_view(), name=SshStorage.add_url_name),
+            SshStorageCreate.as_view(), name=SshStorage.add_url_name),
     re_path(r'^(?P<repo_id>[0-9]+)/storage/ssh/(?P<pk>[0-9]+)/$',
-        SshStorageDetail.as_view(), name=SshStorage.detail_url_name),
+            SshStorageDetail.as_view(), name=SshStorage.detail_url_name),
     re_path(r'^(?P<repo_id>[0-9]+)/storage/ssh/(?P<pk>[0-9]+)/edit/$',
-        SshStorageUpdate.as_view(), name=SshStorage.edit_url_name),
+            SshStorageUpdate.as_view(), name=SshStorage.edit_url_name),
     re_path(r'^(?P<repo_id>[0-9]+)/storage/ssh/(?P<pk>[0-9]+)/delete/$',
-        SshStorageDelete.as_view(), name=SshStorage.delete_url_name),
+            SshStorageDelete.as_view(), name=SshStorage.delete_url_name),
 
     # Git Storage
     re_path(r'^(?P<repo_id>[0-9]+)/storage/git/add/$',
-        GitStorageCreate.as_view(), name=GitStorage.add_url_name),
+            GitStorageCreate.as_view(), name=GitStorage.add_url_name),
     re_path(r'^(?P<repo_id>[0-9]+)/storage/git/(?P<pk>[0-9]+)/$',
-        GitStorageDetail.as_view(), name=GitStorage.detail_url_name),
+            GitStorageDetail.as_view(), name=GitStorage.detail_url_name),
     re_path(r'^(?P<repo_id>[0-9]+)/storage/git/(?P<pk>[0-9]+)/edit/$',
-        GitStorageUpdate.as_view(), name=GitStorage.edit_url_name),
+            GitStorageUpdate.as_view(), name=GitStorage.edit_url_name),
     re_path(r'^(?P<repo_id>[0-9]+)/storage/git/(?P<pk>[0-9]+)/delete/$',
-        GitStorageDelete.as_view(), name=GitStorage.delete_url_name),
+            GitStorageDelete.as_view(), name=GitStorage.delete_url_name),
 ]
 
 if not settings.SINGLE_USER_MODE:
