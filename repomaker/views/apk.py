c63e2869acacac032601f4faf2098a94deadc4e1
thefuture <pmmayero@gmail.com>
2024-10-14 18:17:11 +0300

    Modification to allow multiplefileupload

--- repomaker/views/apk.py
+++ repomaker/views/apk.py
@@ -3,45 +3,13 @@ from django.http import HttpResponseRedirect, HttpResponseNotFound
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView, DeleteView
 
+from multiplefileupload import MultipleFileField, MultipleFileInput, MultipleImageField
 from repomaker.models import Apk, ApkPointer
 from . import BaseModelForm
 from .repository import RepositoryAuthorizationMixin, ApkUploadMixin
 
 
-class MultipleFileInput(ClearableFileInput):
-    allow_multiple_selected = True
-
-
-class MultipleFileField(FileField):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("widget", MultipleFileInput())
-        super().__init__(*args, **kwargs)
-
-    def clean(self, data, initial=None):
-        single_file_clean = super().clean
-        if isinstance(data, (list, tuple)):
-            result = [single_file_clean(d, initial) for d in data]
-        else:
-            result = single_file_clean(data, initial)
-        return result
-
-
-class MultipleImageField(ImageField):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("widget", MultipleFileInput())
-        super().__init__(*args, **kwargs)
-
-    def clean(self, data, initial=None):
-        single_file_clean = super().clean
-        if isinstance(data, (list, tuple)):
-            result = [single_file_clean(d, initial) for d in data]
-        else:
-            result = single_file_clean(data, initial)
-        return result
-
-
 class ApkForm(BaseModelForm):
-    #  apks = FileField(required=False, widget=ClearableFileInput(attrs={'multiple': True}))
     apks = MultipleFileField(required=False)
 
     class Meta:
