0623872311f6355c4188c9e9f7e44f070b1f235e
thefuture <pmmayero@gmail.com>
2024-11-08 15:37:43 +0300

    removed middleware as its not needed for allauht

--- repomaker/settings.py
+++ repomaker/settings.py
@@ -131,8 +131,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # Add the account middleware:
-    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'repomaker.urls'
